2e0e304a80b42107f27d12121b7633ffcf00be60
Ruben-Zamora <ruben.zamora@sciodev.com>
2022-10-06 11:42:52 -0500

    Final
    Final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import { ThemeContext, themes } from "./components/ThemeContext";
+import MyButton from "./components/MyButton";
+import MyNewButton from "./components/MyNewButton";
+
 
 function App() {
+
+  const [currentTheme, setCurrentTheme] =useState(themes.light);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <ThemeContext.Provider value={{
+        theme: currentTheme,
+        updateTheme: setCurrentTheme
+      }}>
+
+        <MyButton onClick={() => setCurrentTheme(themes.dark)}>
+          <Link to="/">Click to view Home page</Link>
+        </MyButton>
+        <MyNewButton onClick={() => setCurrentTheme(themes.light)}>
+          <Link to="about">Click to view About page</Link>
+        </MyNewButton>
+
+      </ThemeContext.Provider>
+
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="about" element={ <About/> } />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
